9da2eab51482bf8a6d37c058cc31b67265859dbe
Farooq Toppa <farooqtoppa@gmail.com>
2017-03-04 11:01:28 -0500

    invoke express app and add mongoose promises

--- server.js
+++ server.js
@@ -8,3 +8,7 @@ var logger          = require('morgan');
 var methodOverride  = require('method-override');
 var passport        = require('passport');
 var localStrategy   = require('passport-local').Strategy;
+var port            = process.env.PORT || 4000;
+var app             = express();
+
+mongoose.Promise = global.Promise;
