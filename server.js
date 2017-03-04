2fb7c4d1ba6cbae6f1161cb3d367bc6e54cf87a8
Farooq Toppa <farooqtoppa@gmail.com>
2017-03-04 10:56:29 -0500

    dependencies for passport required

--- server.js
+++ server.js
@@ -6,3 +6,5 @@ var bodyParser      = require('body-parser');
 var mongoose        = require('mongoose');
 var logger          = require('morgan');
 var methodOverride  = require('method-override');
+var passport        = require('passport');
+var localStrategy   = require('passport-local').Strategy;
